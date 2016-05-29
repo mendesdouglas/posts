cfd572dff1f0a32f5dafe9564b1d0fb5d2ea2e95
Douglas Mendes <lixao_9@hotmail.com>
2016-05-29 09:55:48 -0300

    Atualizacao de padroes para urls e insercao dos html's

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,23 +1,28 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
-
+from .models import Post
 
 def post_create(request):
     return HttpResponse("<h1> create</h1>")
 
 
-def post_detail(request):
+def post_detail(request,id=None):
+    instance = get_object_or_404(Post, id=id)
     context = {
-        "title": "List"
+        "object_list": instance,
+        "instance": instance,
+        "title": instance.title,
     }
-    return render(request, "index.html", context)
+    return render(request, "post_detail.html", context)
     #return HttpResponse("<h1> detail</h1>")
 
 
 def post_list(request):
+    queryset = Post.objects.all()
     context = {
+        "object_list":queryset,
         "title": "List"
     }
     return render(request, "index.html", context)
